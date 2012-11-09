4057ad40ff3ea3d9a7bdb21b07bc5ffcccf728d1
brock a <brock.wright.anderson@gmail.com>
2012-11-09 10:57:01 -0600

    remove object files

--- uC/PIC18/app_lib/TCPIP/Demo App/MPLAB.X/build/C18-PICDN2_ETH97/production/qel_action.o.d
+++
@@ -1,4 +0,0 @@
-build/C18-PICDN2_ETH97/production/qel_action.o : \
-	/home/brock/ethernet-QEL/uC/PIC18/app_lib/TCPIP/Demo\ App/MPLAB.X/qel_action.c \
-	/home/brock/ethernet-QEL/uC/PIC18/app_lib/TCPIP/Demo\ App/MPLAB.X/qel_action.h \
-
