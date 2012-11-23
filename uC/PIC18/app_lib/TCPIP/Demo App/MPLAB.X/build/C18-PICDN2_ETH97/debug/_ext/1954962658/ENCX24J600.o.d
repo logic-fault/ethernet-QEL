5a0645e41c263125b3461108bba16c301e753838
Shreyas <shreyas@ubuntu.(none)>
2012-11-23 13:45:27 -0500

    NFC SPI, to test

--- uC/PIC18/app_lib/TCPIP/Demo App/MPLAB.X/build/C18-PICDN2_ETH97/debug/_ext/1954962658/ENCX24J600.o.d
+++
@@ -1,9 +0,0 @@
-build/C18-PICDN2_ETH97/debug/_ext/1954962658/ENCX24J600.o:  \
-	/home/shreyas/ethernet-QEL/uC/PIC18/app_lib/Microchip/TCPIP\ Stack/ENCX24J600.c  \
-	/opt/microchip/mplabc18/v3.40/h/stdio.h  \
-	/opt/microchip/mplabc18/v3.40/h/stdlib.h  \
-	/opt/microchip/mplabc18/v3.40/h/string.h  \
-	/home/shreyas/ethernet-QEL/uC/PIC18/app_lib/TCPIP/Demo\ App/HardwareProfile.h  \
-	/home/shreyas/ethernet-QEL/uC/PIC18/app_lib/TCPIP/Demo\ App/Configs/HWP\ PICDN2_ETH97.h  \
-	/home/shreyas/ethernet-QEL/uC/PIC18/app_lib/Microchip/Include/Compiler.h  \
-
